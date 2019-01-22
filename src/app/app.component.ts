640ca67a7a5d1a24c62fca308bb6d1f250318920
blackrose <wahomekelving@gmail.com>
2019-01-22 09:19:55 +0300

    Added Users class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GitTracker';
+  users: string[];
+
+  constructor(){
+    this.users = ["Kelvin Wahome"]
+  }
 }
