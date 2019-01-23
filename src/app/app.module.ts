f2a82c248eec5417d261a1b2892f77c376c2d1a6
blackrose <wahomekelving@gmail.com>
2019-01-23 11:20:53 +0300

    Added styling to the repo and form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import { RouterModule, Routes } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { RepositoryComponent } from './repository/repository.component';
 import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     RepositoryComponent,
     UserFormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../user';
+import {InfoService} from '../info.service';
 
 @Component({
-  selector: 'app-user-form',
+  selector: 'app-user',
   templateUrl: './user-form.component.html',
+  providers: [InfoService],
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  userName="";
+  user:User;
+  show=false;
+  show1=false;
+  repos=[];
+  showUser(){
+    this.show=true;
+    this.infoService.infoRequest(this.userName);
+    this.user=this.infoService.user;
+  }
+  showRepo(){
+    this.show1=true;
+    this.infoService.repoRequest(this.userName);
+    this.repos=this.infoService.repos;
+  }
 
-  constructor() { }
+
+  constructor(private infoService:InfoService) { }
 
   ngOnInit() {
   }
